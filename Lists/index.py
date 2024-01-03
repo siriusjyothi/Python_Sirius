5bed64ac87af93e18f982019d221a97d3eebf865
siriusjyothi <siriusjyothi@gmail.com>
2024-01-02 16:08:07 -0800

    m

--- Lists/index.py
+++ Lists/index.py
@@ -1,2 +1,7 @@
 l1 = [10,20,30,40]
-print(l1.index[30])
+print(l1.index(30))
+l2 = [30,10,20,50,10,20]
+print(l2.index(10)) # 1 i.e first indexspot , what if we have duplicate values which index value will it fetch, first index of that element
+# note index(element,start,end) from where to where you want to find the index of partcular element
+print(l2.index(10,4)) # after 4 th index in which spot we have 10 i.e 4th indexspot
+print(l2.index(10,0,5))
